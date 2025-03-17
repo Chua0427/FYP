741c6bb9ad69e777cd4d87b417b6f4399cde725a
Chua0427 <chiannchua05@gmail.com>
2025-03-17 11:35:33 +0800

    update

---
+++ FYP/User/Header_and_Footer/header.php
@@ -0,0 +1,138 @@
+<header>
+<img src="/FYP/img/VeroSports.jpeg" class="logo">
+
+        <div class="subtitleContainer">
+            <div class="subTitle">
+                <ul>
+                    <li id="home">
+                        <a href="test.html">Home</a>
+                    </li>
+                    <li id="newArrival">
+                        <a href="#">New Arrivals</a>
+                    </li>
+                    <div class="dropdown">
+                        <li id="men"><a href="#">Men</a></li>
+                        <div class="dropdownmenu">
+                            <div class="category">
+                                <h3>Footwear</h3>
+                                <a href="#">Boot</a>
+                                <a href="#">Futsal</a>
+                                <a href="#">Running</a>
+                                <a href="#">Court</a>
+                            </div>
+                            <div class="category">
+                                <h3>Apparel</h3>
+                                <a href="#">Jerseys</a>
+                                <a href="#">Jackets</a>
+                                <a href="#">Pants</a>
+                            </div>
+                            <div class="category">
+                                <h3>Equipment</h3>
+                                <a href="#">Bags</a>
+                                <a href="#">Caps</a>
+                                <a href="#">Football Accessories</a>
+                                <a href="#">Socks</a>
+                                <a href="#">Gym Accessories</a>
+                            </div>
+                            <div class="category">
+                                <h3>Shop By Brand</h3>
+                                <a href="#">Nike</a>
+                                <a href="#">Adidas</a>
+                                <a href="#">Puma</a>
+                                <a href="#">Umbro</a>
+                                <a href="#">Lotto</a>
+                                <a href="#">Asics</a>
+                                <a href="#">New Balance</a>
+                                <a href="#">Under Amour</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <li id="women"><a href="#">Women</a></li>
+                        <div class="dropdownmenu">
+                            <div class="category">
+                                <h3>Footwear</h3>
+                                <a href="#">Training</a>
+                                <a href="#">Running</a>
+                                <a href="#">Court</a>
+                            </div>
+                            <div class="category">
+                                <h3>Apparel</h3>
+                                <a href="#">Jerseys</a>
+                                <a href="#">Jackets</a>
+                                <a href="#">Leggings</a>
+                                <a href="#">Pants</a>
+                            </div>
+                            <div class="category">
+                                <h3>Equipment</h3>
+                                <a href="#">Bags</a>
+                                <a href="#">Caps</a>
+                                <a href="#">Socks</a>
+                                <a href="#">Gym Accessories</a>
+                            </div>
+                            <div class="category">
+                                <h3>Shop By Brand</h3>
+                                <a href="#">Nike</a>
+                                <a href="#">Adidas</a>
+                                <a href="#">Puma</a>
+                                <a href="#">Lotto</a>
+                                <a href="#">Asics</a>
+                                <a href="#">New Balance</a>
+                                <a href="#">Skechers</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <li id="kids"><a href="#">Kids</a></li>
+                        <div class="dropdownmenu">
+                            <div class="category">
+                                <h3>Footwear</h3>
+                                <a href="#">Football Shoes</a>
+                                <a href="#">Kids Shoes</a>
+                                <a href="#">School Shoes</a>
+                            </div>
+                            <div class="category">
+                                <h3>Apparel</h3>
+                                <a href="#">Jackets</a>
+                                <a href="#">Jerseys</a>
+                                <a href="#">Paints</a>
+                            </div>
+                            <div class="category">
+                                <h3>Equipment</h3>
+                                <a href="bag">Bag</a>
+                                <a href="#">Caps</a>
+                                <a href="#">Football Accessories</a>
+                                <a href="#">Socks</a>
+                            </div>
+                            <div class="category">
+                                <h3>Shop By Brand</h3>
+                                <a href="#">Nike</a>
+                                <a href="#">Adidas</a>
+                                <a href="#">Puma</a>
+                                <a href="#">Skechers</a>
+                            </div>
+                        </div>
+                    </div>
+                    <li id="promotion">
+                        <a href="#">Promotion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="iconContainer">
+            <div class="icon">
+                <div class="search">
+                    <i class="fa-solid fa-search"></i>
+                </div>
+                <div class="user">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <div class="shoppingCart">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <span id="cartCount">0</span>
+                </div>
+            </div>
+
+        </div>
+    </header>
---
+++ FYP/User/HomePage/homePage.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VeroSports</title>
+    <link rel="stylesheet" href="homePage.css">
+    <link rel="stylesheet" href="../Header_and_Footer/header.css">
+    <link rel="stylesheet" href="../Header_and_Footer/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+</head>
+
+<body>
+<?php include __DIR__ . '/../Header_and_Footer/header.php'; ?>
+
+    <div class="slideshow-container">
+        <i class="fa fa-arrow-left" id="BillboardButton" aria-hidden="true"></i>
+        <div class="slide">
+            <img src="images/img.png" alt="">
+        </div>
+        <div class="slide">
+            <img src="images/Screenshot 2025-02-26 001514.png" alt="">
+        </div>
+        <i class="fa fas fa-arrow-right" id="BillboardNextButton" aria-hidden="true"></i>
+        <div class="slogan-contain">
+            <div class="slogan">
+                <p>VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                    VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                    :VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                    VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORT
+                    VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                    VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                    True
+                    Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="column1">
+        <div class="column">
+            <img src="images/football.png">
+            <p>All Products</p>
+            <input type="button" class="button" value="Buy Now">
+        </div>
+
+        <div class="column">
+            <img src="images/Screenshot 2025-02-26 235034.png">
+            <p>Footware</p>
+            <input type="button" class="button" value="Buy Now">
+        </div>
+        <div class="column">
+            <img src="images/kids.png">
+            <p>Equipment</p>
+            <input type="button" class="button" value="Buy Now">
+        </div>
+    </div>
+
+    <div class="title">
+        <h2>NEW ARRIVALS</h2>
+        <p>"Don't miss out - it is new arrivals product!"</p>
+    </div>
+
+    <div class="NewArrivalsWrapper">
+        <i class="fa fa-arrow-left" id="prevButton" aria-hidden="true"></i>
+
+        <div class="NewArrivalsContainer">
+            <div class="Newcolumn"><img src="images/new.png">
+                <p><input type="button" class="button" value="Shop Now"></p>
+            </div>
+            <div class="Newcolumn"><img src="images/Screenshot 2025-02-27 152344.png">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/bag.png">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/Screenshot 2025-02-27 162528.png">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/Screenshot 2025-02-27 162611.png">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/images.jpeg">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/images.jpeg">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/images.jpeg">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/images.jpeg">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Newcolumn"><img src="images/images.jpeg">
+                <p class="NewArrivalName">Nike</p>
+                <p class="NewArrivalPrice">RM 100</p>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+        </div>
+
+        <i class="fa fa-arrow-right" id="nextButton" aria-hidden="true"></i>
+    </div>
+
+
+    <div class="sub-billboard">
+        <img src="images/Screenshot 2025-03-02 180412.png" alt="">
+        <input type="button" class="button" value="Shop Now">
+    </div>
+
+    <div class="promotionWrapper">
+        <i class="fa fa-arrow-left" id="pButton" aria-hidden="true"></i>
+        <div class="PromotionContainer">
+            <div class="promotion">
+                <img src="images/new.png">
+                <input type="button" class="button" value="Shop Now">
+            </div>
+            <div class="promotion">
+                <div class="discount">30%</div>
+                <img src="images/Screenshot 2025-02-27 152344.png">
+                <p class="promotionName">Nike</p>
+                <span class="promotionPrice">RM 100</span>
+                <span class="discountPrice">RM200</span>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="promotion">
+                <div class="discount">30%</div>
+                <img src="images/bag.png">
+                <p class="promotionName">Nike</p>
+                <span class="promotionPrice">RM 100</span>
+                <span class="discountPrice">RM200</span>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="promotion">
+                <div class="discount">30%</div>
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <p class="promotionName">Nike</p>
+                <span class="promotionPrice">RM 100</span>
+                <span class="discountPrice">RM200</span>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="promotion">
+                <div class="discount">30%</div>
+                <img src="images/Screenshot 2025-02-27 162611.png">
+                <p class="promotionName">Nike</p>
+                <span class="promotionPrice">RM 100</span>
+                <span class="discountPrice">RM200</span>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="promotion">
+                <div class="discount">30%</div>
+                <img src="images/images.jpeg">
+                <p class="promotionName">Nike</p>
+                <span class="promotionPrice">RM 100</span>
+                <span class="discountPrice">RM200</span>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+        </div>
+        <i class="fa fa-arrow-right" id="pnextButton" aria-hidden="true"></i>
+    </div>
+
+    <div class="footballAndGym">
+        <div class="picture">
+            <img src="images/football-training-equipment-7-1024x664.jpg">
+            <input type="button" class="button" value="Shop Now">
+        </div>
+
+        <div class="picture">
+            <img src="images/istockphoto-1287874606-612x612.jpg">
+            <input type="button" class="button" value="Shop Now">
+        </div>
+    </div>
+    <div class="jerseyheader">
+        <img src="images/Screenshot 2025-03-03 143955.png ">
+        <input type="button" class="button" value="Shop Now">
+    </div>
+    <div class="slogan-contain">
+        <div class="slogan">
+            <p>VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                :VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORT
+                VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+                VEROSPORTS:The Real Sports : Unleash Your True Potential : The Power of True Sports : Unleash Your
+                True
+                Potential : Where Passion Meets Performance : Elevate Your Game : Play Hard, Win Real : VEROSPORTS
+            </p>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="columnJersey">
+        <i class="fa fa-arrow-left" id="jButton" aria-hidden="true"></i>
+        <div class="JerseyContainer">
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+            <div class="Jersey">
+                <img src="images/Screenshot 2025-02-27 162528.png">
+                <div class="jerseyname">Football Jersey</div>
+                <div class="jerseyPrice">RM 20</div>
+                <input type="button" class="cartButton" value="Quick Add">
+            </div>
+        </div>
+        <i class="fa fa-arrow-right" id="jnextButton" aria-hidden="true"></i>
+    </div>
+
+    <div class="brandWrapper">
+        <h2>TOP OF BRANDS</h2>
+        <div class="brandContainer">
+            <div class="brand">
+                <a id="nike" href="#"><img src="images/nike.png"></a>
+            </div>
+            <div class="brand">
+                <img src="images/adidas.png">
+            </div>
+            <div class="brand">
+                <img src="images/puma.png">
+            </div>
+            <div class="brand">
+                <img src="images/asics.png">
+            </div>
+            <div class="brand">
+                <img src="images/underAmour.png">
+            </div>
+            <div class="brand">
+                <img src="images/skechers.png">
+            </div>
+            <div class="brand">
+                <img src="images/umbro.png">
+            </div>
+            <div class="brand">
+                <img src="images/Lotto.png">
+            </div>
+        </div>
+
+        <h2 style="background-color: white; margin-top: 100px; ">Shop By Gender</h2>
+        <div class="column1" style="margin-top:1%;">
+            <div class="column" style="width: 400px; height: 600px;">
+                <img src="images/man.jpg">
+                <p>Men</p>
+                <input type="button" class="button" value="Footware">
+                <input type="button" class="button" value="Apparel">
+                <input type="button" class="button" value="Equipment">
+            </div>
+
+            <div class="column" style="width: 400px; height: 600px;">
+                <img src="images/women.jpg">
+                <p>Women</p>
+                <input type="button" class="button" value="Footware">
+                <input type="button" class="button" value="Apparel">
+                <input type="button" class="button" value="Equipment">
+            </div>
+            <div class="column" style="width: 400px; height: 600px;">
+                <img src="images/kids.jpg">
+                <p>Kid</p>
+                <input type="button" class="button" value="Footware">
+                <input type="button" class="button" value="Apparel">
+                <input type="button" class="button" value="Equipment">
+            </div>
+        </div>
+
+        <div class="usWrapper">
+            <h3>Why Choose Us?</h3>
+            <div class="container">
+                <div class="warranty">
+                    <img src="images/warranty.webp">
+                    <p>1 year waranty for products.</p>
+                </div>
+                <div class="payment">
+                    <img src="images/Visa-Logo-2006.png">
+                    <p>Secure payment methods.</p>
+                </div>
+                <div class="delivery">
+                    <img src="images/fast delivery.avif">
+                    <p>Fast deliver < 2 days.</p>
+                </div>
+                <div class="respon">
+                    <img src="images/respons.png">
+                    <p>Support when have problems.</p>
+                </div>
+            </div>
+            <h3></h3>
+        </div>
+
+        <div class="informContainer">
+            <div class="aboutUs">
+                <img src="images/b787ff07-4fea-4f93-bc7c-1f10a1333ee8.webp" alt="">
+                <h3>About Us</h3>
+                <p>Founded in 2025, VEROSPORTS was born out of a passion for sports and a commitment to
+                    innovation.......<a href="#">read more </a></p>
+            </div>
+            <div class="aboutUs">
+                <img src="images/NIK22275_1000_9__48380.jpg" alt="">
+                <h3>Products Warranty</h3>
+                <p>Founded in 2025, VEROSPORTS was born out of a passion for sports and a commitment to
+                    innovation........<a href="#">read more </a></p>
+            </div>
+            <div class="aboutUs">
+                <img src="images/66c063ce-e067-4f24-9ed7-713d6bcf766c.webp" alt="">
+                <h3>Mission And Vision</h3>
+                <p>At VEROSPORTS, our vision is to revolutionize the sports industry by offering top-tier products that
+                    blend performance with style........<a href="#">read more </a></p>
+            </div>
+        </div>
+
+
+        <?php include __DIR__ . '/../Header_and_Footer/header.php'; ?>
+        <script src="homePage.js"></script>
+</body>
+
+</html>
