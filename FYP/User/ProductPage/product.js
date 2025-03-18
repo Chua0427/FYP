6b52ca8621b7f94fdc8745305e3ff442ad4c579c
Chua0427 <chiannchua05@gmail.com>
2025-03-18 15:01:48 +0800

    update product page

--- FYP/User/ProductPage/product.js
+++ FYP/User/ProductPage/product.js
@@ -44,3 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
         mainImage.src = smallImages[currentIndex].src;
     });
 });
+
+
+
+function openModal()
+{
+    document.querySelector(".modal").style.display="flex"
+}
+
+function closeModal()
+{
+    document.querySelector(".modal").style.display="none"
+}
+
+
