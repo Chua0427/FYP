21d753a2f34b8b62c386d63cd30377bcb1b063c8
Chua0427 <chiannchua05@gmail.com>
2025-03-19 15:16:14 +0800

    update

--- FYP/User/All_Product_Page/all_product.js
+++ FYP/User/All_Product_Page/all_product.js
@@ -1,6 +1,19 @@
-let filterbtn = document.getElementById("filterbtn")
-
 function openfilter() {
     let sidebar = document.querySelector(".sidebar");
-    sidebar.style.opacity = "1";
+    let product= document.querySelector(".product-wrapper");
+
+    if(sidebar.style.opacity === "0" || sidebar.style.opacity === "")
+    {
+        sidebar.style.opacity = "1";
+        sidebar.style.visibility= "visible";
+        sidebar.style.width = "250px";
+        sidebar.style.padding = "10px";
+    }
+    else
+    {
+        sidebar.style.opacity = "0";
+        sidebar.style.visibility = "hidden";
+        sidebar.style.width = "0";
+        sidebar.style.padding = "0";
+    }
 }
