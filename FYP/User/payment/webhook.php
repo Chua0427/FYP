7293b2b3f1fbb77bcc22e27020eab5daaab13530
Chua0427 <kaiwen211105@gmail.com>
2025-03-24 12:35:12 +0800

    change

--- FYP/User/payment/webhook.php
+++ FYP/User/payment/webhook.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require __DIR__ . '/../../../vendor/autoload.php';
 \Stripe\Stripe::setApiKey('sk_test_51R3yBQQZPLk7FzRY74j8818Wi2KEMJ4GeHDJAAGhxaEyeFBHEjLrTcY8uzB4v7kvd6yuKCgd4gQIqKArIhfNPIgy00Gktm1Et6');
 
 $payload = @file_get_contents('php://input');
