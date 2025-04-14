2a4d0a3626af4d6b29bad01e3983244a81ae2b0f
Chua0427 <chiannchua05@gmail.com>
2025-04-14 12:06:28 +0800

    1

--- User/login/login.php
+++ User/login/login.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 session_start();
-require_once '/xampp/htdocs/FYP/FYP/vendor/autoload.php';
+require_once '/xampp/htdocs/FYP/vendor/autoload.php';
 require_once __DIR__ . '/../app/init.php';
 require_once __DIR__ . '/../app/auth.php';
 
