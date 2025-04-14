d59dc226a2746bccf984cdbda55ab244211c0de9
Chua0427 <chiannchua05@gmail.com>
2025-04-14 11:17:57 +0800

    1

--- User/All_Product_Page/all_product.php
+++ User/All_Product_Page/all_product.php
@@ -146,12 +146,31 @@
                         }
                     }
 
+                    //sql review
+                            $sql2 = "SELECT 
+                                        p.product_id, 
+                                        AVG(r.rating) as average_rating 
+                                    FROM 
+                                        product p
+                                    LEFT JOIN 
+                                        review r ON p.product_id = r.product_id
+                                    GROUP BY 
+                                        p.product_id";
+
+                            $result2= $conn->query($sql2);
+                            $ratings = [];
+                            while ($row2 = $result2->fetch_assoc()) {
+                                $ratings[$row2['product_id']] = is_null($row2['average_rating']) ? null : round($row2['average_rating']);
+                            }
+                            $user_rating = isset($ratings[$product_id]) ? $ratings[$product_id] : null;
+
                     if ($stock >0) {
                         if($row['status']==='Promotion'){
                             $price = $row['price'];
                             $discount_price = $row['discount_price'];
 
                             $discountPercent = round((($price - $discount_price) / $price) * 100);
+
                             echo '<div class="product-column">
                                 <a href="../ProductPage/product.php?id='.$row['product_id'].'">
                                 <div class="discount">'.$discountPercent.'% OFF</div>
@@ -161,8 +180,22 @@
                                         <span class="price">RM '.$row['discount_price'].'</span>
                                         <span class="discountPrice">RM '.$row['price'].'</span>
                                     </div>
-                                </a>
-                            </div>';
+                                    <div class="rating">';
+
+                                    //star
+                                        for ($i = 1; $i <= 5; $i++) {
+                                            if ($i <= $user_rating) {
+                                                echo '<span class="star filled">&#9733;</span>';
+                                            } else {
+                                                echo '<span class="star">&#9733;</span>';
+                                            }
+                                        }
+                                    
+                                        
+                                        
+                            echo '  </div>
+                                    </a>
+                                </div>';
                         }
                         else{
                             echo '<div class="product-column">
@@ -170,11 +203,22 @@
                                     <img src="../../upload/'.$row['product_img1'].'" alt="">
                                     <p class="product-name">'.$row['product_name'].'</p>
                                     <div class="price">RM'.$row['price'].'</div>
-                                </a>
-                            </div>';
+                                <div class="rating">';
+                                    
+                                //star
+                                    for ($i = 1; $i <= 5; $i++) {
+                                        if ($i <= $user_rating) {
+                                            echo '<span class="star filled">&#9733;</span>';
+                                        } else {
+                                            echo '<span class="star">&#9733;</span>';
+                                        }
+                                    }
+                            
+                            echo '  </div>
+                                    </a>
+                                </div>';
                         }
                     }
-                    
                 }
             ?>
                 
